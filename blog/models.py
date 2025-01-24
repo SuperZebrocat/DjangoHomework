0529b8353b555e7226e5f8d85fb24434bad1dc30
екатерина ермакова <katka_z_jones@mail.ru>
2025-01-24 20:00:19 +0200

    Model Post for app blog created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название статьи')
+    article = models.TextField(verbose_name='Текст статьи')
+    image = models.ImageField(upload_to='images/', verbose_name='Превью', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=False)
+    views = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ['is_published']
+
+    def __str__(self):
+        return self.title
