7a81af4c52a94345a40eb3f44a740024924acd85
екатерина ермакова <katka_z_jones@mail.ru>
2025-01-24 17:35:17 +0200

    Views for app catalog fixed

--- catalog/views.py
+++ catalog/views.py
@@ -1,29 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
 
-
-def home(request):
-    products = Product.objects.all()
-    context = {
-        'products': products
-    }
-    print(context)
-    return render(request, "home.html", context=context)
+from django.views.generic import ListView, DetailView, TemplateView
 
 
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get("name")
-        message = request.POST.get("message")
-        phone = request.POST.get("phone")
-        return HttpResponse(f'Спасибо {name}, мы свяжемся с вами по номеру {phone}')
-    return render(request, "contacts.html")
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
 
 
-def products_detail(request, pk):
-    product = Product.objects.get(pk=pk)
-    context = {
-        'product': product
-    }
-    return render(request, "products_detail.html", context=context)
+class ProductDetailView(DetailView):
+    model = Product
+
+
+class ContactsTemplateView(TemplateView):
+    template_name = 'catalog/contacts.html'
