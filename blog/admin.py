7a9577eab1aeaaf569088be3301192246c38c628
екатерина ермакова <katka_z_jones@mail.ru>
2025-01-26 16:47:35 +0200

    Model Post for app blog registered in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'is_published')
+    list_filter = ('is_published',)
+    search_fields = ('title',)
