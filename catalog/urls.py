0eed7dfc2aa6c170ffa901ddcd46645d64d63427
екатерина ермакова <katka_z_jones@mail.ru>
2025-02-05 08:46:58 +0200

    Urls for CRUD operations for Product model added

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ProductDetailView, ContactsTemplateView
+from catalog.views import ProductListView, ProductDetailView, ContactsTemplateView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('home/', ProductListView.as_view(), name='home'),
     path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
-    path('product/<int:pk>', ProductDetailView.as_view(), name='product_detail')
+    path('products/', ProductListView.as_view(), name='product_list'),
+    path('products/new/', ProductCreateView.as_view(), name='product_create'),
+    path('products/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('products/<int:pk>/update', ProductUpdateView.as_view(), name='product_update'),
+    path('products/<int:pk>/delete', ProductDeleteView.as_view(), name='product_delete'),
 ]
