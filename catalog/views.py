89c503f55f5645add344d297c24fa0d496dca14d
екатерина ермакова <katka_z_jones@mail.ru>
2025-02-05 08:46:10 +0200

    CRUD operations for Product model realized

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Product
+from catalog.forms import ProductForm
+from django.urls import reverse_lazy
+
 
 from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy("catalog:product_list")
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+
+    def get_success_url(self):
+        return reverse_lazy('catalog:product_detail', kwargs={'pk': self.object.pk})
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:product_list')
 
 
 class ProductListView(ListView):
     model = Product
     template_name = 'catalog/home.html'
+
+    def get_queryset(self):
+        return Product.objects.filter(is_published=True)
 
 
 class ProductDetailView(DetailView):
