0e69c78b75935c1cdc05fb0b384ca76d08cbcc7b
екатерина ермакова <katka_z_jones@mail.ru>
2025-02-09 23:59:16 +0200

    Custom views for app users created

--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.core.mail import send_mail
+from django.contrib.auth import login
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    template_name = 'users/register.html'
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        self.send_welcome_email(user.email)
+        return super().form_valid(form)
+
+    def send_welcome_email(self, user_email):
+        subject = 'Добро пожаловать в наш сервис'
+        message = 'Спасибо, что зарегистировались в нашем сервисе!'
+        from_email = 'ekatesting@yandex.ru'
+        recipient_list = [user_email]
+        send_mail(subject, message, from_email, recipient_list)
+
+
+class CustomLoginView(LoginView):
+    form_class = CustomAuthenticationForm
+    template_name = 'users/login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('catalog:product_list')
