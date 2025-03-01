f42c724b2da5cf51d5ada22e7b3a73ed07309dd0
екатерина ермакова <katka_z_jones@mail.ru>
2025-03-01 11:02:49 +0200

    Caches with redis configured

--- config/settings.py
+++ config/settings.py
@@ -111,3 +111,12 @@ SERVER_EMAIL = EMAIL_HOST_USER
 
 LOGIN_REDIRECT_URL = 'catalog:product_list'
 LOGIN_URL = 'users:login'
+
+CACHE_ENABLED = True
+if CACHE_ENABLED:
+    CACHE = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': 'redis://localhost:6379',
+        }
+    }
